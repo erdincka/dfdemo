b3b43bec619c454eae0e03a806d60c8b8c994079
Erdinc Kaya <erdincka@msn.com>
2025-06-28 09:52:55 +0100

    checkpoint

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 sed -i '1,/This container IP/!d' /usr/bin/init-script # remove the while loop at the end
 /usr/bin/init-script
-echo "System initialized, starting demo app..."
+echo "[ $(date) ] System initialized, starting demo app..."
 
 # LD_LIBRARY_PATH=/opt/mapr/lib nohup /root/.local/bin/uv run /app/main.py &
 LD_LIBRARY_PATH=/opt/mapr/lib nohup .venv/bin/streamlit run main.py &
